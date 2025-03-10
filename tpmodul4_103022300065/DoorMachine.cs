3a876a356bc49563d1834cef31ed9c1dc1b90eeb
tjhhh <muhammadfauzan292004@gmail.com>
2025-03-10 14:40:56 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_103022300065/DoorMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300065
+{
+    class DoorMachine
+    {
+        private enum State { TERKUNCI, TERBUKA }
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.TERKUNCI;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void KunciPintu()
+        {
+            if (currentState == State.TERBUKA)
+            {
+                currentState = State.TERKUNCI;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+
+        public void BukaPintu()
+        {
+            if (currentState == State.TERKUNCI)
+            {
+                currentState = State.TERBUKA;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terbuka");
+            }
+        }
+    }
+}
--- tpmodul4_103022300065/Program.cs
+++ tpmodul4_103022300065/Program.cs
@@ -11,5 +11,22 @@ class Program
         string kelurahan = Console.ReadLine();
 
         Console.WriteLine("Kode Pos: " + kodePos.getKodePos(kelurahan));
+        DoorMachine door = new DoorMachine();
+
+        Console.WriteLine("Perintah: (buka/kunci/keluar)");
+        while (true)
+        {
+            Console.Write("Masukkan perintah: ");
+            string perintah = Console.ReadLine();
+
+            if (perintah.ToLower() == "buka")
+                door.BukaPintu();
+            else if (perintah.ToLower() == "kunci")
+                door.KunciPintu();
+            else if (perintah.ToLower() == "keluar")
+                break;
+            else
+                Console.WriteLine("Perintah tidak dikenali!");
+        }
     }
 }
